f681e19d0dc37435a9e95d96468222f5014ad409
neven.shalata <nevenh.1994@gmail.com>
2022-05-06 10:51:28 +0300

    Password validation

---
+++ README.md
@@ -0,0 +1,8 @@
+# password validation
+
+## In this Homework i did a password validation , check the following :
+-  Validate lowercase letters
+- Validate capital letters.
+-  Validate numbers.
+-  Validate length.
+-  
---
+++ js/script.js
@@ -0,0 +1,87 @@
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var symboll=document.getElementById("symbol");
+var threeNumm=document.getElementById("threeNum");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+// validate symbol 
+
+if (/[^A-Za-z0-9]/.test(myInput.value))
+{
+symboll.classList.remove("invalid");
+symboll.classList.add("valid");
+}
+else
+{
+    symboll.classList.remove("valid");
+    symboll.classList.add("invalid");
+}
+
+
+// validate three numbers in Row
+
+if (/(012|123|234|345|456|567|678|789|890|901)/.test(myInput.value))
+{
+    threeNumm.classList.remove("invalid");
+    threeNumm.classList.add("valid");
+}
+else{
+    threeNumm.classList.remove("valid");
+    threeNumm.classList.add("invalid");
+}
+}
+
